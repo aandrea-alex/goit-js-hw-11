b3c7fab89866427d663a37c6059e049b1e8473fd
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2024-07-24 18:11:57 +0000

    Deploying to gh-pages from @ aandrea-alex/goit-js-hw-11@44b859168d077d4e7f926cd798f537d7a8a51bcf 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,9 +1,9 @@
-import{S as h}from"./assets/vendor-5af972a3.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&o(n)}).observe(document,{childList:!0,subtree:!0});function a(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function o(e){if(e.ep)return;e.ep=!0;const s=a(e);fetch(e.href,s)}})();const i="/goit-js-hw-11/assets/icons-f810c139.svg",p="https://pixabay.com/api/",g="45097431-2d8d6a9f4785bbcc4049d8cdd",m=r=>new Promise((t,a)=>fetch(r).then(o=>(o.ok||a(new Error(`HTTP error, status: ${o.status}`)),o.json())).then(o=>t(o)).catch(o=>a(o)));function v(r){const t={key:g,q:encodeURIComponent(r),image_type:"photo",orientation:"horizontal",safesearch:!0,page:1,per_page:20},a=`${p}?${new URLSearchParams(t).toString()}`;return new Promise((o,e)=>{m(a).then(s=>{s.hits.length||e("Sorry, there are no images matching your search query. Please, try again!"),o(s.hits)}).catch(s=>{e(`Error fetching images: ${s.message}`)})})}const y=r=>{const{largeImageURL:t,previewURL:a,tags:o,likes:e,views:s,comments:n,downloads:f}=r;return`<li class='card'> 
+import{S as h,i as p}from"./assets/vendor-8c59ed88.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();const g="/goit-js-hw-11/assets/bi-x-octagon-73f29380.svg";const i="/goit-js-hw-11/assets/icons-f810c139.svg",m="https://pixabay.com/api/",v="45097431-2d8d6a9f4785bbcc4049d8cdd",y=s=>new Promise((t,o)=>fetch(s).then(a=>(a.ok||o(new Error(`HTTP error, status: ${a.status}`)),a.json())).then(a=>t(a)).catch(a=>o(a)));function b(s){const t={key:v,q:encodeURIComponent(s),image_type:"photo",orientation:"horizontal",safesearch:!0,page:1,per_page:20},o=`${m}?${new URLSearchParams(t).toString()}`;return new Promise((a,e)=>{y(o).then(r=>{r.hits.length||e("Sorry, there are no images matching your search query. Please, try again!"),a(r.hits)}).catch(r=>{e(`Error fetching images: ${r.message}`)})})}const w=s=>{const{largeImageURL:t,previewURL:o,tags:a,likes:e,views:r,comments:n,downloads:f}=s;return`<li class='card'> 
               <a class='card-link' href='${t}'>                    
                 <div class='card-cover-inner'>                
                     <img class='card-cover'
-                        src='${a}'
-                        alt='${o}'                                                          
+                        src='${o}'
+                        alt='${a}'                                                          
                     />                                                  
                   <div class='card-cover-darkened'></div>
                 </div>
@@ -15,7 +15,7 @@ import{S as h}from"./assets/vendor-5af972a3.js";(function(){const t=document.cre
                   </div>
                   <div class="field">
                       <span class="label">Views</span>
-                      <span class="value">${s}</span>   
+                      <span class="value">${r}</span>   
                   </div>
                   <div class="field">
                       <span class="label">Comments</span>
@@ -26,12 +26,12 @@ import{S as h}from"./assets/vendor-5af972a3.js";(function(){const t=document.cre
                       <span class="value">${f}</span>    
                   </div>
               </div>
-          </li>`};function b(r){return r.map(y).join("")}function w(r,t){const a=b(r);t.innerHTML="",t.insertAdjacentHTML("beforeend",a)}const L="#ffffff",S="#f5f5f5",c=document.querySelector(".gallery");console.log("galleryRef",c);const l=document.querySelector(".loader"),u=document.querySelector(".search-form");u.addEventListener("submit",$);let d=new h(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,navText:[`<svg class="lightbox-button-icon-prev" width="24" height="24" aria-label="icon arrow left">
+          </li>`};function L(s){return s.map(w).join("")}function S(s,t){const o=L(s);t.innerHTML="",t.insertAdjacentHTML("beforeend",o)}const $="#ffffff",P="#f5f5f5",c=document.querySelector(".gallery"),l=document.querySelector(".loader"),u=document.querySelector(".search-form");u.addEventListener("submit",x);let d=new h(".gallery a",{captionsData:"alt",captionPosition:"bottom",captionDelay:250,nav:!0,navText:[`<svg class="lightbox-button-icon-prev" width="24" height="24" aria-label="icon arrow left">
           <use href="${i}#icon-chevron-left"></use>
         </svg>`,`<svg class="lightbox-button-icon-next" width="24" height="24" aria-label="icon arrow right">
           <use href="${i}#icon-chevron-right"></use>
         </svg>`],close:!0,closeText:`
         <svg class="lightbox-close-icon" width="16" height="16">
           <use href="${i}#close-form"></use>
-        </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0});function $(r){r.preventDefault(),l.style.display="block";const t=r.currentTarget.search.value.trim();v(t).then(a=>x(a)).catch(a=>P())}function P(r){u.search.value="",l.style.display="none",c.style.backgroundColor=S,c.innerHTML="",d.refresh()}function x(r){u.search.value="",l.style.display="none",c.style.backgroundColor=L,console.log("refreshOnSuccess: ",r),w(r,c),d.refresh()}
+        </svg>`,enableKeyboard:!0,showCounter:!0,animationSlide:!0,loop:!0});function x(s){s.preventDefault(),l.style.display="block";const t=s.currentTarget.search.value.trim();b(t).then(o=>O(o)).catch(o=>C(o))}function C(s){u.search.value="",l.style.display="none",R(s),c.style.backgroundColor=P,c.innerHTML="",d.refresh()}function O(s){u.search.value="",l.style.display="none",c.style.backgroundColor=$,S(s,c),d.refresh()}function R(s){p.show({message:s,messageColor:"#fafafb",messageSize:"16px",backgroundColor:"#fc5a5a",position:"topRight",closeOnEscape:!0,close:!0,icon:"Icomoon",iconUrl:`${g}`,iconColor:"#fafafb"})}
 //# sourceMappingURL=commonHelpers.js.map
